5eb1fd7571ecf56a72f34e7ade16ef80cdfa06e4
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-05-16 13:41:56 -0700

    Delete MoodAnalyserMain.java

--- MoodAnalyserMain.java
+++
@@ -1,9 +0,0 @@
-package com.Bridgelabz;
-
-public class MoodAnalyserMain {
-
-    public static void main(String[] args) {
-        MoodAnalyser mood = new MoodAnalyser("I am in Sad Mood");
-        System.out.println(mood.analyseMood());
-    }
-}
