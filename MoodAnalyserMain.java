b32743f097bf07b406448ce8ba27d46eaf8b53b4
swapnil <sopiswarrinku77@gmail.com>
2022-05-17 02:02:17 -0700

    Added : Parameter constructor and modified test casses

---
+++ MoodAnalyserMain.java
@@ -0,0 +1,9 @@
+package com.Bridgelabz;
+
+public class MoodAnalyserMain {
+
+    public static void main(String[] args) {
+        MoodAnalyser mood = new MoodAnalyser("I am in Sad Mood");
+        System.out.println(mood.analyseMood());
+    }
+}
