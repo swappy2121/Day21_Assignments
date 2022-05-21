0030e02ea280530011cee49245ed0c37570053be
PawarSwapnil <102426545+swappy2121@users.noreply.github.com>
2022-05-20 20:36:09 -0700

    Delete MoodAnalysisException.java

--- MoodAnalysisException.java
+++
@@ -1,9 +0,0 @@
-package com.Bridgelabz;
-
-public class MoodAnalysisException extends Exception{
-    String msg;
-
-    public MoodAnalysisException(String msg) {
-        this.msg = msg;
-    }
-}
